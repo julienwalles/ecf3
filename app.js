a04d8e998e59fbe24e51bd27c16aa5c1bf1fbfb6
Julien Walles <j.walles@cci-formation.nc>
2021-05-19 18:10:18 +1100

    ecf3

--- app.js
+++ app.js
@@ -162,7 +162,7 @@ function submit() {
     xhttp.onreadystatechange = function () {
 
         if (this.readyState == 4 && this.status == 200) { 
-              const 
+              
               }
         };
 
